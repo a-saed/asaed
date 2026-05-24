f100b3509cd51114a384d02006ec1a71b047884b
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2026-05-24 16:03:15 +0300

    feat: rss feed at /rss.xml

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server'
+import { posts } from '@/.velite'
+
+const SITE_URL = 'https://asaed.me'
+
+function escapeXml(str: string): string {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+export function GET() {
+  const sorted = [...posts].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+
+  const items = sorted
+    .map(
+      (post) => `
+    <item>
+      <title>${escapeXml(post.title)}</title>
+      <link>${SITE_URL}${post.url}</link>
+      <description>${escapeXml(post.description)}</description>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <guid isPermaLink="true">${SITE_URL}${post.url}</guid>
+    </item>`
+    )
+    .join('')
+
+  const rss = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Abdulrhman Elsaed</title>
+    <link>${SITE_URL}</link>
+    <description>Articles on software engineering and things I learn along the way.</description>
+    <language>en</language>
+    <atom:link href="${SITE_URL}/rss.xml" rel="self" type="application/rss+xml"/>
+    ${items}
+  </channel>
+</rss>`
+
+  return new NextResponse(rss, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' },
+  })
+}
