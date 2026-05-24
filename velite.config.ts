712322317716d5d1f1e0d67258e44c2f8f6ee975
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2026-05-24 15:42:09 +0300

    feat: add velite config, remove contentlayer

---
+++ velite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, s } from 'velite'
+import rehypePrettyCode from 'rehype-pretty-code'
+
+export default defineConfig({
+  root: 'src/_data',
+  output: {
+    data: '.velite',
+    clean: true,
+  },
+  collections: {
+    posts: {
+      name: 'Post',
+      pattern: 'blog/**/*.{md,mdx}',
+      schema: s
+        .object({
+          title: s.string(),
+          date: s.isodate(),
+          description: s.string(),
+          hashnodeSlug: s.string().optional(),
+          tags: s.array(s.string()).optional().default([]),
+          slug: s.path(),
+          content: s.mdx(),
+        })
+        .transform((data) => ({
+          ...data,
+          readingTime: Math.ceil(data.content.raw.split(/\s+/).length / 200),
+          slugParam: data.slug.replace('blog/', ''),
+          url: `/writing/${data.slug.replace('blog/', '')}`,
+        })),
+    },
+  },
+  mdx: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'github-dark-dimmed',
+          keepBackground: false,
+        },
+      ],
+    ],
+  },
+})
