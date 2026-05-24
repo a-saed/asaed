79b8da4e8b0892afd633044c9068c05d85550f9f
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2026-05-24 15:45:11 +0300

    chore: update next and ts config for velite, add /blog redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-const { withContentlayer } = require("next-contentlayer");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog/:slug',
+        destination: '/writing/:slug',
+        permanent: true,
+      },
+    ]
+  },
+  webpack(config) {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  },
+}
 
-module.exports = withContentlayer(nextConfig);
+class VeliteWebpackPlugin {
+  static started = false
+  apply(compiler) {
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      const { build } = await import('velite')
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
+
+module.exports = nextConfig
