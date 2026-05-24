ef4374533059bd7c1e2eb0e469f6a676f8cf7f74
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2026-05-24 15:56:43 +0300

    feat: assemble homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import { Info } from "@/components/Info";
-import { Projects } from "@/components/Projects";
-import { Blog } from "@/components/Blog";
-import { Header } from "@/components/Header";
+import { Nav } from '@/components/nav'
+import { Hero } from '@/components/hero'
+import { WritingList } from '@/components/writing-list'
+import { ProjectGrid } from '@/components/project-grid'
+import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Info />
-      <Projects />
-      <Blog />
-    </main>
-  );
+    <div className="max-w-[680px] mx-auto px-6">
+      <Nav />
+      <Hero />
+      <div className="space-y-16">
+        <WritingList limit={5} />
+        <div id="projects">
+          <ProjectGrid />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
